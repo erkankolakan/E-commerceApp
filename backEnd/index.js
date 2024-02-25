d9e423ea613774e4c51654e984de40a7036bf9b6
Erkan Kolakan <erkankolakan@gmail.com>
2024-02-25 18:06:09 +0300

    controllers sayfasında product ile alakı crud işlemleri yapıldı

---
+++ backEnd/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const app = express();
+
+// router
+const productsRouter = require("./routers/products");
+const port = 3000;
+
+dotenv.config(); //--> .env dosyasına erişir
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+app.use(
+  express.json({
+    limit: "10mb",
+  })
+);
+app.use(
+  express.urlencoded({
+    limit: "10mb",
+    extended: true,
+  })
+)
+
+app.use("/api/products", productsRouter); 
+
+app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${process.env.PORT}!`));
